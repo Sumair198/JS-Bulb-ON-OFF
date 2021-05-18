4ac47b3744c56a0696cf2ba550e20cb9271c9ec5
Sumair198 <sumairk198@gmail.com>
2021-05-18 14:40:19 +0500

    Bulb

---
+++ app.js
@@ -0,0 +1,9 @@
+function off()
+{
+    var a = document.getElementById('ss').src = "img/pic_bulboff.gif"
+}
+
+function on()
+{
+    var a = document.getElementById('ss').src = "img/download.jpg"
+}
